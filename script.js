6abc3457950793f63df4b1e41ee18cb041e44e06
KAUÃ DA SILVA MIRANDA <kaua_miranda@estudante.sesisenai.org.br>
2025-05-22 09:12:01 -0300

    Travei, comentei no codigo pra não me perder, e ta funcionando

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 
+
+// Chama o uuid (tive que pesquisar sobre) pra fazer os id's unicos
 const { v4: uuidv4 } = require('uuid');
 
-// Função que adiciona uma mensagem no arquivo de log
+// é pra adicionar a mensagem do registro no logs.txt
 function adicionarLog(nomeAluno) {
     const idUnico = uuidv4();
+
     const dataHora = new Date().toLocaleString();
+
     const mensagem = `${idUnico} - ${dataHora} - ${nomeAluno}\n`;
+
     fs.appendFile('logs.txt', mensagem, (err) => {
         if (err) {
             console.error('Erro ao escrever no arquivo:', err);
@@ -14,9 +19,10 @@ function adicionarLog(nomeAluno) {
             console.log('Log adicionado com sucesso.');
         }
     });
+    
 }
-
-adicionarLog("Ana");
+//aqui eu coloco o nome do aluno que eu quero adicionar no log
+adicionarLog("Ramon Brignoli");
 
 
 
