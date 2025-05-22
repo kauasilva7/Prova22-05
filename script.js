0fa3e67676336a7576146f33101e63dc4fc2a3d0
KAUÃ DA SILVA MIRANDA <kaua_miranda@estudante.sesisenai.org.br>
2025-05-22 08:37:20 -0300

    Etapa 2 Concluida

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const NotaProva = 0
-console.log(NotaProva)
-const NotaProva2 = 5
-console.log(NotaProva2)
+const fs = require('fs');
+
+const { v4: uuidv4 } = require('uuid');
+
+// Função que adiciona uma mensagem no arquivo de log
+function adicionarLog(nomeAluno) {
+    const idUnico = uuidv4();
+    const dataHora = new Date().toLocaleString();
+    const mensagem = `${idUnico} - ${dataHora} - ${nomeAluno}\n`;
+    fs.appendFile('logs.txt', mensagem, (err) => {
+        if (err) {
+            console.error('Erro ao escrever no arquivo:', err);
+        } else {
+            console.log('Log adicionado com sucesso.');
+        }
+    });
+}
+
+
+adicionarLog("Kauã Silva");
+
+
+
+
