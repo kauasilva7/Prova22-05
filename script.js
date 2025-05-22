965d6b83f06bfe8c3a236e98390e0e36cc78ac74
KAUÃ DA SILVA MIRANDA <kaua_miranda@estudante.sesisenai.org.br>
2025-05-22 10:01:51 -0300

    Fiz oq deu professor, ta pronto oq consegui

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 // Chama o uuid (tive que pesquisar sobre) pra fazer os id's unicos
 const { v4: uuidv4 } = require('uuid');
 
+
 // é pra adicionar a mensagem do registro no logs.txt
 function adicionarLog(nomeAluno) {
     const idUnico = uuidv4();
@@ -12,17 +13,18 @@ function adicionarLog(nomeAluno) {
 
     const mensagem = `${idUnico} - ${dataHora} - ${nomeAluno}\n`;
 
-    fs.appendFile('logs.txt', mensagem, (err) => {
+
+    fs.appendFile('logs.txt', mensagem,'utf8', (err) => {
         if (err) {
             console.error('Erro ao escrever no arquivo:', err);
         } else {
             console.log('Log adicionado com sucesso.');
         }
     });
-    
+
 }
 //aqui eu coloco o nome do aluno que eu quero adicionar no log
-adicionarLog("Ramon Brignoli");
+adicionarLog("Kauã da silva miranda");
 
 
 
